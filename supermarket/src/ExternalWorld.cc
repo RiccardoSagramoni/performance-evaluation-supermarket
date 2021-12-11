a840d444aed134dfd277b35a5f3677b9807f789e
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2021-12-11 16:33:09 +0100

    External world code

--- supermarket/src/ExternalWorld.cc
+++ supermarket/src/ExternalWorld.cc
@@ -4,10 +4,57 @@ Define_Module(ExternalWorld);
 
 void ExternalWorld::initialize()
 {
-    // TODO - Generated method body
+    beep_ = new cMessage("timer");
+
+    // Wait for first arrival
+    wait_new_arrival();
 }
 
 void ExternalWorld::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    generate_new_cart();
+    wait_new_arrival();
 }
+
+void ExternalWorld::generate_new_cart ()
+{
+    // Extract from the chosen distribution how much time
+    // it will take to serve the new cart
+    simtime_t service_time;
+
+    if (service_distribution == 0) { // exponential
+        service_time = exponential(service_mean, SERVICE_RNG);
+    }
+    else if (service_distribution == 1){ // lognormal
+        service_time = lognormal(service_mean, service_std_deviation, SERVICE_RNG);
+    }
+    else { // constant
+        service_time = service_mean;
+    }
+
+    // Send the new cart to the supermarket module,
+    // as a message containing the service time
+    cMessage* msg = new cMessage(SIMTIME_STR(service_time));
+    send(msg, "out");
+    EV << "EXTERNAL_WORLD: generated new cart (" << service_time << ")" << endl;
+}
+
+void ExternalWorld::wait_new_arrival ()
+{
+    // Wait for a random time, extracted from an exponential distribution
+    simtime_t arrival_time = exponential(arrival_mean, ARRIVAL_RNG);
+    scheduleAt(simTime() + arrival_time, beep_);
+    EV << "EXTERNAL_WORLD: next cart arrival in " << arrival_time << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- supermarket/src/ExternalWorld.h
+++ supermarket/src/ExternalWorld.h
@@ -5,12 +5,26 @@
 
 using namespace omnetpp;
 
-/**
- * TODO - Generated class
- */
 class ExternalWorld : public cSimpleModule
 {
-  protected:
+    // Distribution parameters
+    const double arrival_mean = par("arrival_mean");
+    const int service_distribution = par("service_distribution");
+    const double service_mean = par("service_mean");
+    const double service_std_deviation = par("service_std_deviation");
+
+    // RNG
+    const int ARRIVAL_RNG = 0;
+    const int SERVICE_RNG = 1;
+
+    // Messages
+    cMessage* beep_;
+
+    // Methods
+    void generate_new_cart ();
+    void wait_new_arrival ();
+
+protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
 };
