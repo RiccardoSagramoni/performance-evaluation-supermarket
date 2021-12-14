d49ba5a0b5703889c7db16915c66d71bce6343a9
NikoSalamini <50275226+NikoSalamini@users.noreply.github.com>
2021-12-14 19:07:42 +0100

    Till implementation

--- supermarket/src/Till.cc
+++ supermarket/src/Till.cc
@@ -1,14 +1,56 @@
 #include "Till.h"
+#include <queue>
 
 Define_Module(Till);
 
 void Till::initialize()
 {
-    // TODO - Generated method body
-    printf("prova");
+    under_service = false;
+    timer_=new cMessage("beep");
 }
 
 void Till::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    //service time ends
+    if(msg->isSelfMessage()){
+        print_EV("a job has been processed");
+        if(!queue.empty())
+            process_job(NULL);
+        else
+            under_service = false;
+    }
+
+    //new cart arrival
+    else{
+
+        //empty queue
+        if(queue.empty()){
+            process_job(msg);
+        }
+
+        //non-empty queue
+        else{
+            queue.push(msg);
+        }
+    }
+}
+
+int Till::getNumberOfJobs(){
+    return queue.size() + under_service;
+}
+
+void Till::process_job(cMessage* job){
+    if (job == NULL){
+        job = (cMessage*)this->queue.front();
+        queue.pop();
+    }
+    under_service = true;
+    scheduleAt(simTime()+SimTime::parse(job->getClassName()), timer_);
+    delete(job);
+}
+
+void Till::print_EV(const char* str){
+    if(par("isQuick"))
+            EV << "Quick Till["<<this->getId()<<"]: "<<str;
+
 }
--- supermarket/src/Till.h
+++ supermarket/src/Till.h
@@ -2,6 +2,7 @@
 #define __SUPERMARKET_TILL_H_
 
 #include <omnetpp.h>
+#include <queue>
 
 using namespace omnetpp;
 
@@ -10,9 +11,16 @@ using namespace omnetpp;
  */
 class Till : public cSimpleModule
 {
+  std::queue <cMessage*> queue;
+  bool under_service;
+  cMessage* timer_;
+
   protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
+    virtual int getNumberOfJobs();
+    virtual void process_job(cMessage* job);
+    virtual void print_EV(const char* str);
 };
 
 #endif
