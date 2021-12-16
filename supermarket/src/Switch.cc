06c42571dd50ece2c7c10125ea2039830919dcb3
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2021-12-16 16:42:42 +0100

    General fixes
    - Fixed select_till
    - Separati gli output gates in standard e quick
    - Migliorato sistema di routing
    - Fix tipo per parsing di simtime_t

--- supermarket/src/Switch.cc
+++ supermarket/src/Switch.cc
@@ -2,55 +2,61 @@
 
 Define_Module(Switch);
 
-void Switch::initialize(){
+void Switch::initialize() {
 
     // accessing supermarket, that is the parent module
-    cModule *supermarket = getParentModule();
+    cModule* supermarket = getParentModule();
+
     // taking the module pointer of each till
     // and put it in the right vector
-    for(int i = 0; i<num_std_tills; i++){
-        cModule *standard_till = module->getSubmodule("standard_tills", i);
+    for(int i = 0; i < num_std_tills; i++){
+        cModule* standard_till = module->getSubmodule("standard_tills", i);
         standard_tills.push_back((Till*)standard_till);
     }
-    for(int i = 0; i<num_quick_tills; i++){
-        cModule *quick_till = module->getSubmodule("quick_tills", i);
+
+    for(int i = 0; i < num_quick_tills; i++){
+        cModule* quick_till = module->getSubmodule("quick_tills", i);
         quick_tills.push_back((Till*)quick_till);
     }
 
 }
 
-void Switch::handleMessage(cMessage *msg){
+void Switch::handleMessage(cMessage* msg) {
 
-    // index of the till with lowest number of job
-    int index = 0;
-    // extracting the service time of the cart
-    simtime_t *service_time = SimTime::parse(msg->getName());
+    // extract the service time of the cart
+    simtime_t service_time = SimTime::parse(msg->getName());
 
-    if(loggig) {
+    if(logging) {
         EV << "SWITCH: new cart arrived" << service_time << endl;
     }
 
-    // select of the right till, using the threshold as parameter
-    if(service_time < quick_checkout_threshold ){
+    // Select of the right till, using the threshold as parameter
 
-        index = selectTill(quick_tills);
+    if (service_time < quick_checkout_threshold) { // Quick tills
+        // Select index of the till with lowest number of job
+        unsigned int index = selectTill(quick_tills);
+        if(logging) {
+            EV << "SWITCH: selected the QUICK till with lowest response time: " << index << endl;
+        }
+
+        // Send message
+        send(msg, "quick_out", index);
+        if (logging) {
+            EV << "SWITCH: send the cart to the QUICK till n." << index << endl;
+        }
     }
-    else{
+    else { // Standard tills
+        // Select index of the till with lowest number of job
+        unsigned int index = selectTill(standard_tills);
+        if(logging) {
+            EV << "SWITCH: selected the STANDARD till with lowest response time: " << index << endl;
+        }
 
-        index = selectTill(standard_tills);
-    }
-
-    if(console_log) {
-       EV << "SWITCH: selected the till with lowest response time" << endl;
-    }
-
-    // creation of the message to send
-    cMessage* msg = new cMessage(SIMTIME_STR(service_time));
-    // sending the message to the right till
-    send(msg, "out", index);
-
-    if(console_log) {
-        EV << "SWITCH: sent the cart to the till" << endl;
+        // Send message
+        send(msg, "standard_out", index);
+        if (logging) {
+            EV << "SWITCH: send the cart to the STANDARD till n." << index << endl;
+        }
     }
 }
 
@@ -60,18 +66,24 @@ void Switch::handleMessage(cMessage *msg){
  * @vect vector of the tills
  * @return the index of the till in the vector with the lowest number of job
  */
-int Switch::selectTill(vector<Till*> vect){
+unsigned int Switch::selectTill(vector<Till*> vect){
+    
+    if (vect.empty()) {
+        throw "Error: vect can't be empty";
+    }
 
-    int index = 0;
+    unsigned int min_job = vect[0]->get_number_of_job();
+    unsigned int min_job_index = 0;
 
-    for(int i = 0; i < vect.size()-1; i++){
+    for(int i = 1; i < vect.size() - 1; i++){
         // compare each element with the next one
-        if(vect[i+1]->get_number_of_job() < vect[i]->get_number_of_job()){
-            index = i+1;
+        if(vect[i]->get_number_of_job() < min_job){
+            min_job_index = i;
+            min_job = vect[i]->get_number_of_job();
         }
     }
 
-    return index;
+    return min_job_index;
 }
 
 
