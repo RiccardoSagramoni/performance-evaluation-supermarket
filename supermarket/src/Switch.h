c561ab74825b7207b8f8cdb2ea0ec74b7a376a55
Fabrizio Lanzillo <fabriziolanzillo@gmail.com>
2021-12-16 12:55:50 +0100

    update switch

--- supermarket/src/Switch.h
+++ supermarket/src/Switch.h
@@ -11,29 +11,19 @@ using namespace omnetpp;
 class Switch : public cSimpleModule{
 
 
-    // service distribution collecato alla trashhold?
     const int percentage_quick_tills = par("percentage_quick_tills");
     const int quick_checkout_threshold = par("quick_checkout_threshold");
     const int number_of_tills = par("number_of_tills");
-    const bool console_log = par("console_log");
+    const bool logging = par("logging");
 
-    // Messages
-    cMessage* beep_ = nullptr;
 
-    //coda di backlog?
-
-    int num_quick_tills = floor(number_of_tills*percentage_quick_tills);
+    int num_quick_tills = floor(number_of_tills * percentage_quick_tills);
     int num_std_tills = number_of_tills - num_quick_tills;
 
+    vector<Till*> quick_tills;
+    vector<Till*> standard_tills;
 
-    int tills[number_of_tills];
-    int index_quick = number_of_tills- num_quick_tills;
-
-
-    // Methods
-    void cart_routing(cMessage *msg);
-    int getIndexTill(int array[], int dimension);
-
+    int selectTill(vector<Till*> vect);
 
     protected:
         virtual void initialize();
