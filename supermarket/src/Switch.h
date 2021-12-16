e3f662cbac9eae0dde89f73db6b24af80d7ddb49
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2021-12-16 16:42:42 +0100

    General fixes
    - Fixed select_till
    - Separati gli output gates in standard e quick
    - Migliorato sistema di routing
    - Fix tipo per parsing di simtime_t

--- supermarket/src/Switch.h
+++ supermarket/src/Switch.h
@@ -10,24 +10,27 @@ using namespace omnetpp;
 
 class Switch : public cSimpleModule{
 
-
+    // Parameters for the till
     const int percentage_quick_tills = par("percentage_quick_tills");
     const int quick_checkout_threshold = par("quick_checkout_threshold");
     const int number_of_tills = par("number_of_tills");
-    const bool logging = par("logging");
-
 
     int num_quick_tills = floor(number_of_tills * percentage_quick_tills);
     int num_std_tills = number_of_tills - num_quick_tills;
+    
+    // Enable/disable logging
+    const bool logging = par("logging");
 
-    vector<Till*> quick_tills;
-    vector<Till*> standard_tills;
+    // Pointers to the tills
+    std::vector<Till*> quick_tills;
+    std::vector<Till*> standard_tills;
 
-    int selectTill(vector<Till*> vect);
+    // Methods
+    unsigned int selectTill(std::vector<Till*> vect);
 
-    protected:
-        virtual void initialize();
-        virtual void handleMessage(cMessage *msg);
+protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage* msg);
 };
 
 #endif
