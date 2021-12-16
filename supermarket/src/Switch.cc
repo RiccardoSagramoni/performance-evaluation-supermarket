43d288f03e4941a8b54520242a85f7e24531ae5a
Fabrizio Lanzillo <fabriziolanzillo@gmail.com>
2021-12-16 13:23:32 +0100

    aggiunta di alcuni commenti

--- supermarket/src/Switch.cc
+++ supermarket/src/Switch.cc
@@ -4,7 +4,10 @@ Define_Module(Switch);
 
 void Switch::initialize(){
 
+    // accessing supermarket, that is the parent module
     cModule *supermarket = getParentModule();
+    // taking the module pointer of each till
+    // and put it in the right vector
     for(int i = 0; i<num_std_tills; i++){
         cModule *standard_till = module->getSubmodule("standard_tills", i);
         standard_tills.push_back((Till*)standard_till);
@@ -18,6 +21,7 @@ void Switch::initialize(){
 
 void Switch::handleMessage(cMessage *msg){
 
+    // index of the till with lowest number of job
     int index = 0;
     // extracting the service time of the cart
     simtime_t *service_time = SimTime::parse(msg->getName());
@@ -25,9 +29,6 @@ void Switch::handleMessage(cMessage *msg){
     if(loggig) {
         EV << "SWITCH: new cart arrived" << service_time << endl;
     }
-
-    // creation of the message to send to the right till
-    cMessage* msg = new cMessage(SIMTIME_STR(service_time));
 
     // select of the right till, using the threshold as parameter
     if(service_time < quick_checkout_threshold ){
@@ -43,7 +44,9 @@ void Switch::handleMessage(cMessage *msg){
        EV << "SWITCH: selected the till with lowest response time" << endl;
     }
 
-    // sending the message on the right till
+    // creation of the message to send
+    cMessage* msg = new cMessage(SIMTIME_STR(service_time));
+    // sending the message to the right till
     send(msg, "out", index);
 
     if(console_log) {
@@ -51,12 +54,18 @@ void Switch::handleMessage(cMessage *msg){
     }
 }
 
+/**
+ * Select the till with the lowest number of job
+ *
+ * @vect vector of the tills
+ * @return the index of the till in the vector with the lowest number of job
+ */
 int Switch::selectTill(vector<Till*> vect){
 
     int index = 0;
 
     for(int i = 0; i < vect.size()-1; i++){
-
+        // compare each element with the next one
         if(vect[i+1]->get_number_of_job() < vect[i]->get_number_of_job()){
             index = i+1;
         }
