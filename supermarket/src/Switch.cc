5d1450caba4a88fe4ad7f0e00143d8d7f6e94239
Fabrizio Lanzillo <fabriziolanzillo@gmail.com>
2021-12-16 11:20:26 +0100

    switch update

--- supermarket/src/Switch.cc
+++ supermarket/src/Switch.cc
@@ -2,12 +2,85 @@
 
 Define_Module(Switch);
 
-void Switch::initialize()
-{
-    // TODO - Generated method body
+void Switch::initialize(){
+
+    beep_ = new cMessage("timer");
 }
 
-void Switch::handleMessage(cMessage *msg)
-{
-    // TODO - Generated method body
+void Switch::handleMessage(cMessage *msg){
+
+    // timeout? if we put it we should use a backlog?
+    cMessage *msg = receive();
+
+    if (msg==nullptr){
+
+        //handle error?
+    }
+    else if(msg->isSelfMessage()){
+
+        // togli una quantità equivalente di service time a quell'index
+    }
+    else{
+        if (dynamic_cast<simtime_t *>(msg) != nullptr){
+            simtime_t *service_time = (simtime_t *)msg;
+
+            if(console_log) {
+                EV << "SWITCH: new cart arrived" << service_time << endl;
+            }
+
+            receive_new_cart(service_time);
+        }
+    }
 }
+
+
+void Switch::cart_routing(simtime_t service_time){
+
+    int dimension = 0;
+    int index = 0;
+    int selected_till_index = 0;
+
+    if(service_time < percentage_quick_tills){
+
+        dimension = this->num_quick_tills;
+        index = this->index_quick;
+    }
+    else{
+        dimension = this->num_standard_tills;
+        index = 0;
+    }
+
+
+    if(console_log) {
+        EV << "SWITCH: selecting the quick till with lowest response time" << endl;
+    }
+
+    selected_till_index = getIndexTill(dimension,index);
+    this->tills[selected_till_index] += service_time;
+
+    scheduleAt(simTime() + service_time, beep_);
+
+    cMessage* msg = new cMessage(SIMTIME_STR(service_time));
+    send(msg, "out[selected_till_index]");
+
+    if (console_log) {
+        EV << "SWITCH: cart (" << service_time << ") has been routed" << endl;
+    }
+
+}
+
+int Switch::getIndexTill(int dimension, int index){
+
+    int min_index_responsetime_till= 0;
+
+    for(int i=index; i< dimension-1; i++){
+        if(this->tills[i+1] < this->tills[i]){
+            min_index_responsetime_till = i+1;
+        }
+    }
+
+    return min_index_responsetime_till;
+}
+
+
+
--- supermarket/src/Switch.h
+++ supermarket/src/Switch.h
@@ -2,17 +2,42 @@
 #define __SUPERMARKET_SWITCH_H_
 
 #include <omnetpp.h>
+#include <vector>
+#include <tgmath.h>
 
 using namespace omnetpp;
 
-/**
- * TODO - Generated class
- */
-class Switch : public cSimpleModule
-{
-  protected:
-    virtual void initialize();
-    virtual void handleMessage(cMessage *msg);
+
+class Switch : public cSimpleModule{
+
+
+    // service distribution collecato alla trashhold?
+    const int percentage_quick_tills = par("percentage_quick_tills");
+    const int quick_checkout_threshold = par("quick_checkout_threshold");
+    const int number_of_tills = par("number_of_tills");
+    const bool console_log = par("console_log");
+
+    // Messages
+    cMessage* beep_ = nullptr;
+
+    //coda di backlog?
+
+    int num_quick_tills = floor(number_of_tills*percentage_quick_tills);
+    int num_std_tills = number_of_tills - num_quick_tills;
+
+
+    int tills[number_of_tills];
+    int index_quick = number_of_tills- num_quick_tills;
+
+
+    // Methods
+    void cart_routing(cMessage *msg);
+    int getIndexTill(int array[], int dimension);
+
+
+    protected:
+        virtual void initialize();
+        virtual void handleMessage(cMessage *msg);
 };
 
 #endif
