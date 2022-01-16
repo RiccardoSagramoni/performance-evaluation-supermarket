16bbd0c31c71d45581db153cf03daf0572a5c9ac
NikoSalamini <50275226+NikoSalamini@users.noreply.github.com>
2022-01-16 16:19:10 +0100

    Added compute_entire_response_time.py and compute_entire_response_time_repeat.py

--- python/compute_entire_response_time_repeat.py
+++ python/compute_entire_response_time_repeat.py
@@ -36,40 +36,42 @@ def compute_confidence_interval(alpha, std, size):
     return std * norm.ppf(1 - alpha / 2) / np.sqrt(size)
 
 FILE = ["response_time", "waiting_time"]
+directory_contents = os.listdir("./../supermarket/2kr")
 
-for z in range(0, 2):
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), "./../supermarket/2kr/P=-1_K=-1_AM=-1_SM=-1/"+FILE[z]+".csv"), sep=';')
-    for i in range(0, len(df.columns), 2):
-        if df.columns[i].find("#0") != -1:
-            reps_values[0] = reps_values[0] + df.iloc[:, i + 1].values.tolist()
-        elif df.columns[i].find("#1") != -1:
-            reps_values[1] = reps_values[1] + df.iloc[:, i + 1].values.tolist()
-        elif df.columns[i].find("#2") != -1:
-            reps_values[2] = reps_values[2] + df.iloc[:, i + 1].values.tolist()
-        elif df.columns[i].find("#3") != -1:
-            reps_values[3] = reps_values[3] + df.iloc[:, i + 1].values.tolist()
-        elif df.columns[i].find("#4") != -1:
-            reps_values[4] = reps_values[4] + df.iloc[:, i + 1].values.tolist()
-        else:
-            sys.exit("error")
+for h in range(len(directory_contents)):
+    for z in range(0, 2):
+        df = pd.read_csv(os.path.join(os.path.dirname(__file__), "./../supermarket/2kr/"+directory_contents[h]+"/"+FILE[z]+".csv"), sep=';')
+        for i in range(0, len(df.columns), 2):
+            if df.columns[i].find("#0") != -1:
+                reps_values[0] = reps_values[0] + df.iloc[:, i + 1].values.tolist()
+            elif df.columns[i].find("#1") != -1:
+                reps_values[1] = reps_values[1] + df.iloc[:, i + 1].values.tolist()
+            elif df.columns[i].find("#2") != -1:
+                reps_values[2] = reps_values[2] + df.iloc[:, i + 1].values.tolist()
+            elif df.columns[i].find("#3") != -1:
+                reps_values[3] = reps_values[3] + df.iloc[:, i + 1].values.tolist()
+            elif df.columns[i].find("#4") != -1:
+                reps_values[4] = reps_values[4] + df.iloc[:, i + 1].values.tolist()
+            else:
+                sys.exit("error")
 
-    csv_data = {}
-    total_jobs = [pd.DataFrame(reps_values[0]), pd.DataFrame(reps_values[1]), pd.DataFrame(reps_values[2]),
-                  pd.DataFrame(reps_values[3]), pd.DataFrame(reps_values[4])]
+        csv_data = {}
+        total_jobs = [pd.DataFrame(reps_values[0]), pd.DataFrame(reps_values[1]), pd.DataFrame(reps_values[2]),
+                      pd.DataFrame(reps_values[3]), pd.DataFrame(reps_values[4])]
 
-    #compute the mean, std, conf_int for each repetition
-    for i in range(0, 5):
-        total_jobs[i] = total_jobs[i].dropna()
-        [mean, std] = compute_avg_and_std(total_jobs[i].values)
-        conf_int = compute_confidence_interval(0.01, std, len(total_jobs[i]))
-        mean_list.append(mean)
-        std_list.append(std)
-        conf_list.append(conf_int)
+        #compute the mean, std, conf_int for each repetition
+        for i in range(0, 5):
+            total_jobs[i] = total_jobs[i].dropna()
+            [mean, std] = compute_avg_and_std(total_jobs[i].values)
+            conf_int = compute_confidence_interval(0.01, std, len(total_jobs[i]))
+            mean_list.append(mean)
+            std_list.append(std)
+            conf_list.append(conf_int)
 
-    #print(total_jobs[0]) gli index che stampa sono quelli delle vecchie righe che sono rimaste dopo il dropna
-    csv_data["mean"] = mean_list
-    csv_data["std"] = std_list
-    csv_data["conf_int"] = conf_list
-    data = pd.DataFrame(data=csv_data)
-    data.to_csv(os.path.join(os.path.dirname(__file__),
-                             "./../supermarket/2kr/P=-1_K=-1_AM=-1_SM=-1/"+FILE[z]+"_out.csv"), sep=';')
+        #print(total_jobs[0]) gli index che stampa sono quelli delle vecchie righe che sono rimaste dopo il dropna
+        csv_data["mean"] = mean_list
+        csv_data["std"] = std_list
+        csv_data["conf_int"] = conf_list
+        data = pd.DataFrame(data=csv_data)
+        data.to_csv(os.path.join(os.path.dirname(__file__),
+                                 "./../supermarket/2kr/"+directory_contents[h]+"/"+FILE[z]+"_out.csv"), sep=';')
