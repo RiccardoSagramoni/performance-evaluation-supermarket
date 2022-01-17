3a4d333d1bdafcfdef76387366d8490af91fe929
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-01-17 13:34:44 +0100

    Add comments and fix indentation

--- python/FACTORIAL-ANALYSIS/generate_sign_matrix.py
+++ python/FACTORIAL-ANALYSIS/generate_sign_matrix.py
@@ -1,7 +1,12 @@
 import csv
+import os
+import os.path
 
 v = [-1, 1]
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# Write the sign matrix for the factorial analysis
+# This configuration is for 5 factors
 with open("sign.csv", "w", newline='') as file:
 	writer = csv.writer(file, delimiter=";")
 
